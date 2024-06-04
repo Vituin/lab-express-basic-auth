04433b55802519cb49752654dcb2ab6b56a9798d
Vituin <124085647+Vituin@users.noreply.github.com>
2024-06-04 17:54:37 +0200

    no solved

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const router = require("express").Router();
+const User = require(`../models/User.model`)
+const bcrypt = require(`bcryptjs`)
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render(`auth/register`);
 });
+
+router.post(`/register`, async (req, res) => {
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(req.body.password, salt)
+  const newUser = {
+    username: req.body.username,
+    password: hashedPassword
+  }
+  User.create(newUser).then((data) => {
+    res.render(`index`, { user: data })
+  })
+    .catch((error) => res.json(error))
+})
 
 module.exports = router;
